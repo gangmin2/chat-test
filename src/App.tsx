0fb814e9022f86bdd64f4a74dfacf12092b62747
gangmin2 <min0115b@gmail.com>
2024-06-12 23:45:46 +0900

    stomp js 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './App.css'
+import { Client } from '@stomp/stompjs';
+
+const BASE_URI: string = 'ws://localhost:8080';
+const TOKEN: string = 'accessToken';
+const workspaceId: number = 1;
+const username: string = 'user';
 
 // 메시지 타입을 정의하는 인터페이스
 interface Message {
@@ -25,9 +31,19 @@ const MESSAGES: Message[] = [
     "senderName": "sender"
   },
   {
+    "messageType": "ENTER",
+    "message": "sender2 님이 입장했습니다.",
+    "senderName": "sender2",
+  },
+  {
     "messageType": "TALK",
     "message": "message",
     "senderName": "sender2"
+  },
+  {
+    "messageType": "EXIT",
+    "message": "sender2 님이 퇴장했습니다.",
+    "senderName": "sender2",
   },
 ]
 
@@ -43,7 +59,7 @@ const Bubble: React.FC<BubbleProps> = ({ messageType, message, senderName }) => 
     return <p>{message}</p>;
   }
   if (messageType === 'TALK') {
-    return <div style={{ margin: '16px 0'}} >
+    return <div style={{ margin: '16px 0' }} >
       <p style={{ margin: '0', }}>{senderName}</p>
       <p style={{ backgroundColor: '#eee', width: 'fit-content', padding: '4px 8px', borderRadius: '8px', margin: '0', }}>{message}</p>
     </div>;
@@ -52,6 +68,53 @@ const Bubble: React.FC<BubbleProps> = ({ messageType, message, senderName }) => 
 };
 
 const App: React.FC = () => {
+  const [messages, setMessages] = useState<Message[]>(MESSAGES);
+  const [inputMessage, setInputMessage] = useState('');
+
+  const client = new Client({
+    brokerURL: `${BASE_URI}/api/ws`,
+    connectHeaders: {
+      Authorization: TOKEN,
+    },
+    reconnectDelay: 5000,
+
+    onConnect: () => {
+      client.subscribe(`/api/sub/${workspaceId}`, message => {
+        if (message.body) {
+          const newMessage: Message = {
+            messageType: 'TALK',
+            message: message.body,
+            senderName: 'username',
+          };
+          setMessages((prevMessages) => [...prevMessages, newMessage]);
+        } else {
+          alert('got empty message');
+        }
+      });
+      client.publish({
+        destination: `/api/pub/${workspaceId}`,
+        body: 'First Message',
+        headers: { Authorization: TOKEN },
+      });
+    },
+  });
+
+  const sendMessage = () => {
+    if (inputMessage.trim() !== '') {
+      client.publish({
+        destination: `/api/pub/${workspaceId}`,
+        body: JSON.stringify({
+          messageType: 'TALK',
+          message: inputMessage,
+          senderName: username,
+        }),
+        headers: { Authorization: TOKEN },
+      });
+
+      setInputMessage('');
+    }
+  };
+  
   return (
     <div>
       <div>
@@ -61,18 +124,31 @@ const App: React.FC = () => {
       <input type="text" name="" id="" placeholder="Search" />
 
       <div>
-        {MESSAGES.map((msg, index) => (
-          <Bubble
-            key={index}
-            messageType={msg.messageType}
-            message={msg.message}
-            senderName={msg.senderName}
-          />
-        ))}
+        {messages.length === 0 ? (
+          <p>채팅을 시작해보세요 :)</p>
+        ) : (
+          messages.map((msg, index) => (
+            <Bubble
+              key={index}
+              messageType={msg.messageType}
+              message={msg.message}
+              senderName={msg.senderName}
+            />
+          ))
+        )}
       </div>
-      
-      <form>
-        <input type="text" name="message" placeholder='채팅을 입력하세요' />
+
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        sendMessage();
+      }} >
+        <input
+          type="text"
+          name="message"
+          placeholder='채팅을 입력하세요'
+          value={inputMessage}
+          onChange={(e) => setInputMessage(e.target.value)}
+        />
         <input type="submit" value="SEND" />
       </form>
     </div>
