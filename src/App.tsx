b9f108de087b271df0582bce9a61cb94e5626e47
gangmin2 <min0115b@gmail.com>
2024-06-12 18:07:26 +0900

    채팅 UI 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,82 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+// 메시지 타입을 정의하는 인터페이스
+interface Message {
+  messageType: 'TALK' | 'ENTER' | 'EXIT';
+  message: string;
+  senderName: string;
 }
+
+const MESSAGES: Message[] = [
+  {
+    "messageType": "ENTER",
+    "message": "sender 님이 입장했습니다.",
+    "senderName": "sender",
+  },
+  {
+    "messageType": "TALK",
+    "message": "message",
+    "senderName": "sender"
+  },
+  {
+    "messageType": "TALK",
+    "message": "message",
+    "senderName": "sender"
+  },
+  {
+    "messageType": "TALK",
+    "message": "message",
+    "senderName": "sender2"
+  },
+]
+
+// Bubble 컴포넌트의 prop 타입을 정의하는 인터페이스
+interface BubbleProps {
+  messageType: 'TALK' | 'ENTER' | 'EXIT';
+  message: string;
+  senderName: string;
+}
+
+const Bubble: React.FC<BubbleProps> = ({ messageType, message, senderName }) => {
+  if (messageType === 'ENTER' || messageType === 'EXIT') {
+    return <p>{message}</p>;
+  }
+  if (messageType === 'TALK') {
+    return <div style={{ margin: '16px 0'}} >
+      <p style={{ margin: '0', }}>{senderName}</p>
+      <p style={{ backgroundColor: '#eee', width: 'fit-content', padding: '4px 8px', borderRadius: '8px', margin: '0', }}>{message}</p>
+    </div>;
+  }
+  return null;
+};
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <div>
+        채팅 (<span>4</span>)
+      </div>
+
+      <input type="text" name="" id="" placeholder="Search" />
+
+      <div>
+        {MESSAGES.map((msg, index) => (
+          <Bubble
+            key={index}
+            messageType={msg.messageType}
+            message={msg.message}
+            senderName={msg.senderName}
+          />
+        ))}
+      </div>
+      
+      <form>
+        <input type="text" name="message" placeholder='채팅을 입력하세요' />
+        <input type="submit" value="SEND" />
+      </form>
+    </div>
+  );
+};
 
 export default App
