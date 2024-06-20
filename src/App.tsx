319d37fdb0d4ec7133e7cd6575a0f45d6863bc77
gangmin2 <min0115b@gmail.com>
2024-06-21 02:35:49 +0900

    Answer/Offer 주고받기

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,8 @@
 import React, { FormEvent, useEffect, useRef, useState } from 'react';
 import { Client, IMessage } from '@stomp/stompjs';
-import AudioCapture from './AudioCapture';
 
 const BASE_URI: string = 'ws://localhost:8080';
 const workspaceId: number = 1;
-
-interface Participant {
-  id: string;
-  name: string;
-}
 
 interface Message {
   messageType: 'TALK' | 'ENTER' | 'EXIT';
@@ -22,16 +16,22 @@ interface BubbleProps {
   senderName: string;
 }
 
+interface WebRTCSignalDTO {
+  peerId: string;
+  sdp?: string;
+  candidate?: string;
+}
+
 const Bubble: React.FC<BubbleProps> = ({ messageType, message, senderName }) => {
   if (messageType === 'ENTER' || messageType === 'EXIT') {
     return <p>{message}</p>;
   }
   if (messageType === 'TALK') {
     return (
-      <div style={{ margin: '16px 0' }}>
-        <p style={{ margin: '0' }}>{senderName}</p>
-        <p style={{ backgroundColor: '#eee', width: 'fit-content', padding: '4px 8px', borderRadius: '8px', margin: '0' }}>{message}</p>
-      </div>
+        <div style={{ margin: '16px 0' }}>
+          <p style={{ margin: '0' }}>{senderName}</p>
+          <p style={{ backgroundColor: '#eee', width: 'fit-content', padding: '4px 8px', borderRadius: '8px', margin: '0' }}>{message}</p>
+        </div>
     );
   }
   return null;
@@ -42,28 +42,16 @@ const App: React.FC = () => {
   const [inputMessage, setInputMessage] = useState(''); // 사용자가 입력한 채팅 메시지
   const [isConnected, setIsConnected] = useState(false); // WebSocket 연결 상태
   const [subscriberCount, setSubscriberCount] = useState(0); // 현재 채팅을 구독하고 있는 사용자 수
-  // const [isMuted, setIsMuted] = useState(false); // 로컬 마이크의 음소거 상태
-
   const clientRef = useRef<Client | null>(null); // WebSocket 클라이언트를 참조
+
   const localStreamRef = useRef<MediaStream | null>(null); // 로컬 오디오 스트림을 참조
   const remoteStreams = useRef<{ [key: string]: MediaStream }>({}); // 원격 사용자 오디오 스트림 참조
   const peerConnections = useRef<{ [key: string]: RTCPeerConnection }>({}); // 피어 연결 관리 객체 참조
 
-  const [participants, setParticipants] = useState<Participant[]>([]);
+  const [peerId, setPeerId] = useState<string>(''); // 고유한 peerId 생성
 
-  // 예를 들어, WebSocket을 통해 새로운 참가자가 들어왔을 때 participants 상태를 업데이트할 수 있습니다.
   useEffect(() => {
-    // WebSocket 또는 다른 방법으로 새로운 참가자가 들어왔을 때 participants를 업데이트하는 로직을 작성합니다.
-    // 예시로 초기에 몇 명의 참가자를 가정합니다.
-    setParticipants([
-      { id: '1', name: 'Participant 1' },
-      { id: '2', name: 'Participant 2' },
-    ]);
-  }, []);
-
-  // WebSocket 클라이언트 초기화 및 연결 설정
-  useEffect(() => {
-    const client = new Client({
+    const client: any = new Client({
       brokerURL: `${BASE_URI}/api/ws`,
       debug: (str) => {
         console.debug(str);
@@ -71,7 +59,7 @@ const App: React.FC = () => {
       reconnectDelay: 5000,
       heartbeatIncoming: 4000,
       heartbeatOutgoing: 4000,
-      onConnect: () => handleWebSocketConnect(client),
+      onConnect: (frame) => handleWebSocketConnect(client, frame),
       onDisconnect: () => handleWebSocketDisconnect(client),
       onStompError: (frame) => {
         console.error('Broker reported error: ' + frame.headers['message']);
@@ -83,6 +71,7 @@ const App: React.FC = () => {
       onWebSocketClose: () => {
         console.log('WebSocket closed');
         setIsConnected(false);
+        handleWebSocketDisconnect(client);
       },
       beforeConnect: () => {
         console.log('Attempting to connect...');
@@ -92,26 +81,35 @@ const App: React.FC = () => {
     client.activate();
     clientRef.current = client;
 
+    // Handle window unload event
+    const handleBeforeUnload = () => {
+      if (clientRef.current) {
+        clientRef.current.publish({
+          destination: `/api/pub/webrtc/${workspaceId}/leave`,
+          body: JSON.stringify({ peerId }),
+        });
+      }
+    };
+
+    window.addEventListener('beforeunload', handleBeforeUnload);
+
     return () => {
       if (clientRef.current) {
         clientRef.current.deactivate();
       }
+      window.removeEventListener('beforeunload', handleBeforeUnload);
     };
   }, []);
 
-  // WebSocket 연결 성공 시 처리
-  const handleWebSocketConnect = (client: Client) => {
+  const handleWebSocketConnect = (client: Client, frame: any) => {
     setIsConnected(true); // WebSocket 연결 상태를 true로 설정
+    const sessionId = frame.headers['user-name'];
+    setPeerId(sessionId);
+    console.log(`Connected with session ID: ${sessionId}`);
 
     client.subscribe(`/api/sub/chat/${workspaceId}`, (message: IMessage) => {
       const newMessage = JSON.parse(message.body) as Message;
       setMessages((prevMessages) => [...prevMessages, newMessage]);
-
-      const newParticipant = JSON.stringify({
-        id: '3',
-        name: 'Participant 3',
-      })
-      setParticipants((prevParticipants) => [...prevParticipants, newParticipant]);
     });
 
     client.subscribe(`/api/sub/chat/${workspaceId}/count`, (message: IMessage) => {
@@ -119,22 +117,32 @@ const App: React.FC = () => {
     });
 
     client.subscribe(`/api/sub/webrtc/${workspaceId}/offer`, (message: IMessage) => {
-      handleReceiveOffer(JSON.parse(message.body));
+      console.log('Received offer', message.body);
+      handleReceiveOffer(JSON.parse(message.body) as WebRTCSignalDTO);
     });
 
     client.subscribe(`/api/sub/webrtc/${workspaceId}/answer`, (message: IMessage) => {
-      handleReceiveAnswer(JSON.parse(message.body));
+      console.log('Received answer', message.body);
+      handleReceiveAnswer(JSON.parse(message.body) as WebRTCSignalDTO);
     });
 
     client.subscribe(`/api/sub/webrtc/${workspaceId}/ice-candidate`, (message: IMessage) => {
-      handleReceiveIceCandidate(JSON.parse(message.body));
+      console.log('Received ICE candidate', message.body);
+      handleReceiveIceCandidate(JSON.parse(message.body) as WebRTCSignalDTO);
+    });
+
+    client.subscribe(`/api/sub/webrtc/${workspaceId}/peers`, (message: IMessage) => {
+      const peers = JSON.parse(message.body) as string[];
+      console.log('Updated peer list', peers);
+      callAllUsers(peers);
     });
 
     client.publish({
       destination: `/api/pub/chat/${workspaceId}`,
       body: JSON.stringify({
         messageType: 'ENTER',
-        message: ''
+        message: '',
+        senderName: sessionId
       }),
     });
 
@@ -143,32 +151,43 @@ const App: React.FC = () => {
       body: '',
     });
 
-    // 로컬 오디오 스트림을 시작하고, 다른 사용자들에게 음성 통화 요청
-    startLocalStream().then(() => {
-      // 다른 사용자에게 통화 시작 알림
-      callAllUsers();
+    // 서버에 현재 피어 ID 등록
+    client.publish({
+      destination: `/api/pub/webrtc/${workspaceId}/join`,
+      body: JSON.stringify({ peerId: sessionId }),
     });
+
+    // 로컬 오디오 스트림을 시작합니다.
+    startLocalStream();
   };
 
-  // WebSocket 연결 종료 시 처리
   const handleWebSocketDisconnect = (client: Client) => {
     setIsConnected(false); // WebSocket 연결 상태를 false로 설정
     stopLocalStream(); // 로컬 오디오 스트림 정지
-    client.deactivate(); // WebSocket 클라이언트 비활성화하여 연결 종료
+
+    // 서버에 현재 피어 ID 등록 해제
+    if (client) {
+      console.log(`Sending leave message for peerId: ${peerId}`);
+      client.publish({
+        destination: `/api/pub/webrtc/${workspaceId}/leave`,
+        body: JSON.stringify({ peerId }),
+      });
+
+      client.deactivate(); // WebSocket 클라이언트 비활성화하여 연결 종료
+    }
   };
 
-  // 로컬 오디오 스트림 가져오기
   const startLocalStream = async () => {
     try {
       const localStream = await navigator.mediaDevices.getUserMedia({ audio: true }); // 사용자에게 오디오 접근 권한을 요청
       localStreamRef.current = localStream;
-    } catch (error) {
+      console.log('Local stream started');
+    } catch (error: any) {
       console.error('Error accessing local media:', error);
       alert(`Error accessing local media: ${error.message}`);
     }
   };
 
-  // 로컬 오디오 스트림 정지
   const stopLocalStream = () => {
     if (localStreamRef.current) {
       localStreamRef.current.getTracks().forEach(track => track.stop());
@@ -176,9 +195,7 @@ const App: React.FC = () => {
     }
   };
 
-  // 피어 연결 설정 및 ICE candidate 이벤트 핸들링 (특정 사용자와의 피어 연결을 설정)
   const setupPeerConnection = (peerId: string) => {
-    // RTCPeerConnection 객체를 생성 및 ICE 서버 설정
     const peerConnection = new RTCPeerConnection({
       iceServers: [
         { urls: 'stun:stun.l.google.com:19302' },
@@ -187,9 +204,10 @@ const App: React.FC = () => {
 
     peerConnections.current[peerId] = peerConnection;
 
-    peerConnection.onicecandidate = event => {
+    peerConnection.onicecandidate = (event: RTCPeerConnectionIceEvent) => {
       if (event.candidate) {
-        sendMessage(`/api/pub/webrtc/${workspaceId}/ice-candidate`, JSON.stringify({ peerId, candidate: event.candidate }));
+        console.log(`ICE candidate generated for peer ${peerId}:`, event.candidate);
+        sendMessage(`/api/pub/webrtc/${workspaceId}/ice-candidate`, JSON.stringify({ peerId: peerId, candidate: JSON.stringify(event.candidate), sdp: '' }));
       }
     };
 
@@ -202,11 +220,13 @@ const App: React.FC = () => {
         document.body.appendChild(remoteAudio);
         remoteAudio.srcObject = remoteStreams.current[peerId];
       }
+      console.log(`Track received from peer ${peerId}:`, event.track);
       remoteStreams.current[peerId].addTrack(event.track);
     };
 
     if (localStreamRef.current) {
       localStreamRef.current.getTracks().forEach(track => {
+        console.log(`Adding track to peer ${peerId}:`, track);
         peerConnection.addTrack(track, localStreamRef.current!);
       });
     }
@@ -214,66 +234,53 @@ const App: React.FC = () => {
     return peerConnection;
   };
 
-  // Offer 수신 처리
-  const handleReceiveOffer = async (data: { peerId: string, sdp: RTCSessionDescriptionInit }) => {
+  const handleReceiveOffer = async (data: WebRTCSignalDTO) => {
     const { peerId, sdp } = data;
+    console.log(`Handling offer from peer ${peerId}`, data);
     const peerConnection = setupPeerConnection(peerId); // 피어 연결 설정
-    await peerConnection.setRemoteDescription(new RTCSessionDescription(sdp)); // SDP 설정
+    await peerConnection.setRemoteDescription(new RTCSessionDescription({ type: 'offer', sdp: sdp! })); // SDP 설정
     const answer = await peerConnection.createAnswer();  // answer 생성 및 상대방에게 전송
     await peerConnection.setLocalDescription(answer);
-    sendMessage(`/api/pub/webrtc/${workspaceId}/answer`, JSON.stringify({ peerId, sdp: answer }));
+    sendMessage(`/api/pub/webrtc/${workspaceId}/answer`, JSON.stringify({ peerId, sdp: answer.sdp, candidate: '' }));
   };
 
-  // Answer 수신 처리
-  const handleReceiveAnswer = async (data: { peerId: string, sdp: RTCSessionDescriptionInit }) => {
+  const handleReceiveAnswer = async (data: WebRTCSignalDTO) => {
     const { peerId, sdp } = data;
+    console.log(`Handling answer from peer ${peerId}`, data);
     const peerConnection = peerConnections.current[peerId];
-    await peerConnection.setRemoteDescription(new RTCSessionDescription(sdp)); // Remote SDP 설정
+    await peerConnection.setRemoteDescription(new RTCSessionDescription({ type: 'answer', sdp: sdp! })); // Remote SDP 설정
   };
 
-  // ICE candidate 수신 처리
-  const handleReceiveIceCandidate = async (data: { peerId: string, candidate: RTCIceCandidateInit }) => {
+  const handleReceiveIceCandidate = async (data: WebRTCSignalDTO) => {
     const { peerId, candidate } = data;
+    console.log(`Handling ICE candidate from peer ${peerId}`, data);
     const peerConnection = peerConnections.current[peerId];
-    await peerConnection.addIceCandidate(new RTCIceCandidate(candidate)); // ICE candidate 추가
+    await peerConnection.addIceCandidate(new RTCIceCandidate(JSON.parse(candidate!))); // ICE candidate 추가
   };
 
-  // 메시지 전송
   const sendMessage = (destination: string, body: string) => {
     if (clientRef.current && isConnected) {
+      console.log(`Sending message to ${destination}:`, body);
       clientRef.current.publish({ destination, body });
     }
   };
 
-  // 모든 사용자에게 통화 요청 보내기
-  const callAllUsers = async () => {
-    //  애플리케이션 상태 또는 백엔드 엔드포인트에서 연결된 피어 ID 목록을 검색
-    const peerIds = Object.keys(peerConnections.current);
-    for (const peerId of peerIds) {
-      callUser(peerId);
+  const callAllUsers = async (peerIds: string[]) => {
+    console.log('Calling all users:', peerIds);
+    for (const id of peerIds) {
+      if (id !== peerId) {
+        callUser(id);
+      }
     }
   };
 
-  // 특정 사용자에게 통화 요청 보내기
   const callUser = async (peerId: string) => {
-    // 해당 사용자의 PeerConnection을 설정하고, Offer를 전송하여 통화 요청
     const peerConnection = setupPeerConnection(peerId);
     const offer = await peerConnection.createOffer();
     await peerConnection.setLocalDescription(offer);
-    sendMessage(`/api/pub/webrtc/${workspaceId}/offer`, JSON.stringify({ peerId, sdp: offer }));
+    sendMessage(`/api/pub/webrtc/${workspaceId}/offer`, JSON.stringify({ peerId: peerId, sdp: offer.sdp, candidate: '' }));
   };
 
-  // 마이크 음소거 토글
-  // const toggleMute = () => {
-  //   if (localStreamRef.current) {
-  //     localStreamRef.current.getAudioTracks().forEach(track => {
-  //       track.enabled = !track.enabled;
-  //     });
-  //     setIsMuted(!isMuted);
-  //   }
-  // };
-
-  // 채팅 메시지 전송
   const handleSendMessage = (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     if (!inputMessage.trim() || !isConnected || !clientRef.current) {
@@ -284,7 +291,8 @@ const App: React.FC = () => {
       destination: `/api/pub/chat/${workspaceId}`,
       body: JSON.stringify({
         messageType: 'TALK',
-        message: inputMessage
+        message: inputMessage,
+        senderName: peerId
       }),
     });
 
@@ -292,48 +300,42 @@ const App: React.FC = () => {
   };
 
   return (
-    <div>
       <div>
         <div>
-          채팅 (<span>{subscriberCount}</span>)
+          <div>
+            채팅 (<span>{subscriberCount}</span>)
+          </div>
+
+          <input type="text" name="" id="" placeholder="Search" />
+
+          <div>
+            {messages.length === 0 ? (
+                <p>채팅을 시작해보세요</p>
+            ) : (
+                messages.map((msg, index) => (
+                    <Bubble
+                        key={index}
+                        messageType={msg.messageType}
+                        message={msg.message}
+                        senderName={msg.senderName}
+                    />
+                ))
+            )}
+          </div>
+
+          <form onSubmit={handleSendMessage}>
+            <input
+                type="text"
+                name="message"
+                placeholder="채팅을 입력하세요"
+                value={inputMessage}
+                onChange={(e) => setInputMessage(e.target.value)}
+            />
+            <input type="submit" value="SEND" />
+          </form>
         </div>
-
-        <input type="text" name="" id="" placeholder="Search" />
-
-        <div>
-          {messages.length === 0 ? (
-            <p>채팅을 시작해보세요</p>
-          ) : (
-            messages.map((msg, index) => (
-              <Bubble
-                key={index}
-                messageType={msg.messageType}
-                message={msg.message}
-                senderName={msg.senderName}
-              />
-            ))
-          )}
-        </div>
-
-        <form onSubmit={handleSendMessage}>
-          <input
-            type="text"
-            name="message"
-            placeholder="채팅을 입력하세요"
-            value={inputMessage}
-            onChange={(e) => setInputMessage(e.target.value)}
-          />
-          <input type="submit" value="SEND" />
-        </form>
+        <div id="remoteAudios"></div>
       </div>
-
-      {participants.map(participant => (
-        <div key={participant.id}>
-          <h3>{participant.name}</h3>
-          <AudioCapture key={participant.id} /> {/* 각 참가자에 대해 AudioCapture 컴포넌트를 렌더링 */}
-        </div>
-      ))}
-    </div>
   );
 };
 
